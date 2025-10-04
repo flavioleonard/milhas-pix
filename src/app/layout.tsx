82e6fb91abf089fcc17708e852ecec159f465ef4
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2025-10-04 09:32:30 -0300

    style: add font mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { DM_Sans } from 'next/font/google';
+import { DM_Sans, DM_Mono } from 'next/font/google';
 import { Layout } from '../components/layout/Layout';
 import './globals.css';
 
@@ -8,13 +8,19 @@ const dmSans = DM_Sans({
   variable: '--font-dm-sans',
 });
 
+const dmMono = DM_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  variable: '--font-dm-mono',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR" className={dmSans.variable}>
+    <html lang="pt-BR" className={`${dmSans.variable} ${dmMono.variable}`}>
       <body className={dmSans.className}>
         <Layout>
           {children}
